0859baa3bf77a73d3ec031478e1ceaa68d0b9cb3
Petr Savrasov <54024241+Pete8219@users.noreply.github.com>
2020-01-17 17:58:46 +0500

    Commit on 17 january

--- radio.js
+++ radio.js
@@ -1,12 +1,177 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
     let data = {};
+    
 
-    function GetData() {
+    let rating = [
+        {criteria:'Приезжают быстро, устраняют причину аварии качественно',grade: 5},
+        {criteria:'Приезжают быстро, но устраняют причины аварии некачественно, приходится вызывать снова',grade: 3},
+        {criteria:'Приезжают в течение суток после множества звонков', grade: 1},
+        {criteria:'Не дозвониться, не приезжают, ни чего не делают', grade: 0},
+        {criteria:'Мусора нет, крылечки и тротуары очищены от снега',grade: 5},
+        {criteria:'Мусор и снег убирают раз в неделю',grade: 3},
+        {criteria:'Не придёт, пока не позвонишь в компанию',grade: 1},
+        {criteria:'Ни разу его не видели, убираем сами',grade: 0},
+        {criteria:'Колеи и сугробов нет, убирают и вывозят снег регулярно',grade: 5},
+        {criteria:'Убирают и вывозят снег раз в месяц',grade: 3},
+        {criteria:'Убирают и вывозят снег 2-3 раза за зиму',grade: 1},
+        {criteria:'Ни разу не убирали и не вывозили снег',grade: 0},
+        {criteria:'Регулярно меняют лампочки, ремонтируют или регулируют входные двери, ремонтируют перила и ступеньки',grade: 5},
+        {criteria:'Меняют лампочки, ремонтируют входные двери, перила и ступени в случае неоднократного обращения',grade: 3},
+        {criteria:'Меняют лампочки, ремонтируют входные двери, перила и ступени только в случае значительного износа',grade: 1},
+        {criteria:'Ни чего не делают, просить бесполезно',grade: 0},
+        {criteria:'Ремонт делают периодически по мере необходимости. Нареканий к качеству нет',grade: 5},
+        {criteria:'Ремонт делают переодически по мере необходимости. Есть существенные замечания к качеству',grade: 3},
+        {criteria:'Ремонт делают после неоднократных обращений, только в случае, если есть значительный износ',grade: 1},
+        {criteria:'Мы сами делаем косметический ремонт, просить компанию нет смысла',grade: 0},
+        {criteria:'Есть стенд, на котором указана вся необходимая информация',grade: 5},
+        {criteria:'Есть стенд, но половина информации отсутствует или уже устарела',grade: 3},
+        {criteria:'Стенд размещён не удобно, приходится искать информацию',grade: 1},
+        {criteria:'Стенд отсутствует',grade: 0},
+        {criteria:'Регулярно проводят собрания, активно взаимодействуют с жителями в соц.сетях и мессенджерах',grade: 5},
+        {criteria:'Редко проводят собрания, принимают только письменные завления в офисе и по электронной почте и телефону',grade: 3},
+        {criteria:'Собрания не проводят, принимают заявления только в офисе, отвечают на заявки и вопросы только по телефону.',grade: 1},
+        {criteria:'Не взаимодействуют',grade: 0},
+        {criteria:'Разговаривают вежливо, предоставляю развернутую информацию по работе управляющей компании',grade: 5},
+        {criteria:'Разговаривают вежливо, но информацию предоставляют только по запросу',grade: 3},
+        {criteria:'Игнорируют',grade: 1},
+        {criteria:'Разговаривают очень грубо',grade: 0},
+        {criteria:'Ни разу не обращался поскольку компания работает хорошо',grade: 5},
+        {criteria:'Не обращался, но есть претензии к качеству работы',grade: 3},
+        {criteria:'Не обращался, но претензии есть',grade: 1},
+        {criteria:'Обращаюсь регулярно – не реже 1 раза в месяц',grade: 0},
+        {criteria:'Предоставляет, работает в соответствии с планом',grade: 5},
+        {criteria:'Предоставляет, но значительная часть планов не выполнена',grade: 3},
+        {criteria:'Размещает на стенде без обсуждения с жителями',grade: 1},
+        {criteria:'Не предоставляет',grade: 0},
+
+
+    ]
+
+
+    let uprCompany = {
+        company:'',
+    }
+
+    let params = {
+        company: [
+            "",
+            "ООО ЕГУК",
+            "ООО Стройжилсервис",
+            "ООО Изумрудный город",
+            "ООО СБИ",
+            "ТСЖ Изумрудный город",
+            "ТСЖ Возрождение",
+            "ООО ЖК Возрождение",
+            "ООО Квартал",
+            "ООО Жилстандарт",
+            "ООО Кит",
+            "ТСЖ Ямальский дом",
+            "ТСЖ Наш дом",
+            "ООО УК Доверие",
+            "ООО КЦ ЯНАО",
+            "ООО Салехардская жилищная компания"
+        ],
+        
+        questions: [
+            "",
+            "Как вы оцениваете качество и скорость работы аварийной службы?",
+            "Как вы оцениваете работу дворника?",
+            "Как вы оцениваете качество механизированной очистки двора от снега и вывоз снега?",
+            "Как вы оцениваете работу по содержанию мест общего пользования в доме (подъезды, коридоры, крыльца, лестницы и т.п.)?",
+            "Как вы оцениваете своевременность и качество проведения управляющей компанией косметического ремонта (покраска стен, полов, перил, дверей и оконных рам, побелка стен и потолка, замена листов и т.д.) в местах общего пользования (коридоры, подъезды)?",
+            "Установлены ли в местах общего пользования дома информационные стенды с актуальной информацией об управляющей компании (реквизиты, адрес,тел.офиса, эл.почта и т.п.), телефонах экстренных служб, аварийно-диспетчерской службы, перечне работ и услуг по содержанию и ремонту общего имущества и пр.",
+            "Как вы оцениваете взаимодействие управляющей компании с жильцами?",
+            "Как вы оцениваете вежливость сотрудников при общении с жильцами?",
+            "Как часто вам приходилось обращаться в надзорные органы и различные инстанции с жалобами на плохую работу управляющей компании?",
+            "Предоставляет ли УК и ТСЖ информацию о планах работы на год и отчёт о проделанной работе для обсуждения?"
+
+        ]
+    };
+
+
+    let requestParam = {
+        uk: '',
+        criteriy: ''
+    };
+
+    let companyName = document.getElementById('ratingUK');
+    let question = document.getElementById('questions');
+
+    /*  Создание выпадающего списка управляющих компаний в элементе select */
+
+    function createSelect() {
+        items = params.company;
+        quest = params.questions;
+         
+            items.forEach(function(item) {
+                let option = document.createElement("option");
+                
+                option.value = item;
+                option.text = item;
+                companyName.add(option);
+            
+            } )
+
+            quest.forEach(function(qItem) {
+                let option = document.createElement("option");
+                option.value = qItem;
+                option.text = qItem;
+                question.add(option);
+                
+            })
+    }
+
+
+
+
+    createSelect();
+
+    let name, valueOption;
+    // Отслеживание события выбора в списке
+
+       companyName.addEventListener('change', function () {
+            let name = document.getElementById('ratingUK').options.selectedIndex;
+            let valueOption = document.getElementById('ratingUK').options[name].value;
+            requestParam.uk = valueOption.split(" ").join("-");  
+    })
+
+    // Отслеживание события выбора в списке
+
+        question.addEventListener('change', function () {
+            let QName = document.getElementById('questions').options.selectedIndex;
+            let QValue = document.getElementById('questions').options[QName].value; 
+            requestParam.criteriy = QValue.split(" ").join("-");
+    })
+
+
+    // Отслеживание нажатие кнопки Сформировать отчет
+
+    let btn = document.getElementById('results');
+
+        btn.addEventListener('click', function() {
+            let uParam = requestParam.uk;
+            let qParam = requestParam.criteriy;
+
+            if (uParam == null && qParam == null) {
+                console.log('Вы не выбрали ни один из критериев отбора');
+            }
+            
+
+            GetData(uParam, qParam);
+        })
+
+
+    
+
+    /* Получение данных в формате JSON из файла */    
+
+    function GetData(uParam, qParam) {
     
         let request = new XMLHttpRequest();
 
-        request.open('GET', '111.json');
+        request.open('GET', '111.json');    //файл с данными 
+
         request.setRequestHeader('Content-Type','application/json');
 
         request.send();
@@ -14,56 +179,102 @@ document.addEventListener("DOMContentLoaded", ()=>{
         request.addEventListener('readystatechange', function() {
             if (request.readyState === 4 && request.status == 200) {
                 data = JSON.parse(request.response);
+               //console.log(data);  
           
-                let radio_info = document.querySelector('.radio-info');
-                console.log(data);
-               
-
-                radio_info.innerHTML =
-                 
-                `<div style='float:left;'> IP:</div>     ${data[0]['Выберите управляющую компанию']} </br>`; /*
-+
-                `<div style='float:left;'> Location:</div>     ${data.icestats.location} </br>` +
-                `<div style='float:left;'>Host radio:</div>     ${data.icestats.host}</br>`;
-*/
+                let table = document.querySelector('table');
 
 
+               /*  function GetCompanyResults(results) {
+                    if (results) {
 
-  /*               function goRec(obj) {
-                    
-                    for (let key in obj) {
+                    } else {
                         
-                        let value = obj[key];
-                        if (typeof value !=="object"){
-                        console.log(`key: ${key}, value: ${value}`);
-                        }
+                    }
 
-                        if(typeof value == "object") {
-                            goRec(value);
-                        }
+
+
+                } */
+                
+               
+                 /* Генерация таблицы */
+                
+                let tData = Object.keys(data[0]);
+                
+                
+                generateTableHead(table,tData); 
+                generateTable(table, data);
+
+                function generateTableHead(table,tData) {   //создание заголовка таблицы
+                    let thead = table.createTHead();
+                    let row = thead.insertRow();
+                    for (let key of tData) {
+
+                        let th = document.createElement("th");
+                        let text = document.createTextNode(key);
+                        
+                            
+                            th.appendChild(text);
+                                                
+                        row.appendChild(th);
                     }
                 }
- */
-                /* goRec(data);  */
+
+                function generateTable(table, tData) {      // создание табличной части
+                    for (let element of tData) {
+
+                     
+                        
+
+                            
+                           // console.log(element["Выберите управляющую компанию"]);
+                            
+        
+                        
+                        
+                       
+                        let row = table.insertRow();
+                        for (key in element) {
+                            
+                            
+                            let cell = row.insertCell();
+                            let text = document.createTextNode(element[key].toLowerCase());
+                            
+                            let newText = element[key].split(" ").join("-");
+                     
+                        /*Функция преобразование текстовых ответов в цифровую форму*/
+                        
+                        function replaceText() {
+                            rating.forEach(function(item) {
+                                if (newText === item.criteria.split(" ").join("-")) {
+                                    newText = item.grade;
+                                    text = document.createTextNode(newText);
+                                } 
+                                
+                            })
+                        }
+                        
+                        replaceText();
+                        cell.appendChild(text);
+                        }
+                    
+                    }
+                } 
+
 
             }
- 
+          /*   PullData(data); */
+            
         });
         
         
            
     }
     
-    GetData();
-
-/*
-    function setTime (){
-
-        setInterval(GetData, 2000);
-
-    }
-
-    setTime();
-*/
+    
+    /* GetData(); */
+/*     function PullData(data) {
+        console.log(data[0].IP);
+    } */
+    
 
 });
